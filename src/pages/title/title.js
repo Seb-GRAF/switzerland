acc39fe1ec011fc3633874695e2812ef268f5732
MorePog <95282692+MorePog@users.noreply.github.com>
2022-02-26 23:55:39 +0100

    bug fixes

--- src/pages/title/title.js
+++ src/pages/title/title.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./style.scss";
 
 const Title = ({ children }) => {
-  return <div className="title">{children}</div>;
+  return (
+    <div className="title">
+      {children}
+      <p data-scroll data-scroll-speed="1">
+        ↓
+      </p>
+    </div>
+  );
 };
 
 export default Title;
